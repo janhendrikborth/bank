922a17da46aba5ff2cdff977593b8a669c33eb9c
janhendrikborth <jan-hendrik.borth@accenture.com>
2016-09-21 14:42:24 +0200

    Delete mysql.sql

--- mysql.sql
+++
@@ -1,3 +0,0 @@
-create database bankapp;
-create table accounts (id int not null auto_increment, accountNumber int not null, balance int, primary key(id))
-insert into accounts(accountNumber, balance) values (100, 50);
