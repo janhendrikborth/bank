e7160ea455ee7c0eab2a961e1b1965b8aacc6559
jan-hendrik.borth <jan-hendrik.borth@accenture.com>
2016-09-21 12:09:06 +0200

    new sql

---
+++ mysqlnew.sql
@@ -0,0 +1,4 @@
+create database bankapp;
+use bankapp;
+create table accounts (id int not null auto_increment, accountNumber int not null, balance int, primary key(id));
+insert into accounts(accountNumber, balance) values (100, 50);
